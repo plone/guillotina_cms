7b02b21ac8fc7cc2cb5402132cfd89eaa19b8ca9
Ramon Navarro Bosch <ramon.nb@gmail.com>
2018-09-19 21:08:45 +0200

    Title from resource

--- guillotina_cms/content/gcontent.py
+++ guillotina_cms/content/gcontent.py
@@ -2,7 +2,7 @@ from guillotina.behaviors.dublincore import IDublinCore
 from guillotina.interfaces import IResource
 from guillotina_cms import directives
 
-directives.fieldset_field.apply(IDublinCore, 'title', 'default')  # pylint: disable=E1101
+directives.fieldset_field.apply(IResource, 'title', 'default')  # pylint: disable=E1101
 directives.fieldset_field.apply(IDublinCore, 'description', 'default')  # pylint: disable=E1101
 directives.fieldset_field.apply(IDublinCore, 'effective_date', 'dates')  # pylint: disable=E1101
 directives.fieldset_field.apply(IDublinCore, 'expiration_date', 'dates')  # pylint: disable=E1101
